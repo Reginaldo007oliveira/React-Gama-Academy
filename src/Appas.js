c0e039c1b736616917568bf42bc3ffcb940a721d
Atacadoweb <atacadowebonline@gmail.com>
2021-07-07 17:47:29 -0300

    Propriedades, estados e propriedades base do react

--- src/Appas.js
+++ src/Appas.js
@@ -1,12 +1,16 @@
+//Usando Hooks: estado das coisas
+import React, { Fragment, useState } from "react";
 import "./App.css";
 function Appas(props) {
+  //declara a constante usuário e atribui um valor do estado para ela
+  //conceito de desestruturação
+  const [usuario, setUsuario] = useState("Guilherme");
   return (
-    //div pai com um elemento h1 uma div interna e um input
-    <div>
-      <h1>
-        {props.title}
-        {props.name}
-      </h1>
+    //div pai com um elemento h1 uma div interna e um input <div> foi substituida por <></>
+    <Fragment>
+      <p>{usuario}</p>
+      <h1>{props.title}</h1>
+      <h2> {props.name}</h2>
       <div>DIGITE O NOME:</div>
       <input
         name="Usuários"
@@ -14,7 +18,7 @@ function Appas(props) {
         className="usuarioInput"
         placeholder="Usuário"
       />
-    </div> // fim da div pai
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Appas from "./Appas";
-ReactDOM.render(<Appas title="Hello Gama " />, document.getElementById("root"));
+ReactDOM.render(
+  <Appas title="Hello Gama " name="Bora bora.. " />,
+  document.getElementById("root")
+);
