73528e7d9304a3a11f16a46df2e7c89d86588973
Atacadoweb <atacadowebonline@gmail.com>
2021-07-07 16:08:31 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Appas from "./Appas";
+ReactDOM.render(
+  <Appas title="Hello Gama A" />,
+  document.getElementById("root")
+);
